17f76b34f8ce39568bec535a389799ee43ef6638
Brian Guarraci <brian.guarraci@gmail.com>
2012-07-12 23:00:11 -0700

    update

--- load.sh
+++ load.sh
@@ -1,1 +1,1 @@
-java -jar target/tweeder-0.0.1-SNAPSHOT-jar-with-dependencies.jar tweets ../stored.io/tweets.json
+java -jar target/tweeder-0.0.1-SNAPSHOT-jar-with-dependencies.jar tweets tweets.json
